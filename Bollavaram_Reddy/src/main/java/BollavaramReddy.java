497f774eb1a1ac3ccaa6cf96772191667409b8b7
Bollavaram Sai Haranatha Reddy <bollavaramsaiharanatha.reddy@publicissapient.com>
2022-04-05 17:17:01 +0530

    Sending file to repo

---
+++ Bollavaram_Reddy/src/main/java/BollavaramReddy.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class BollavaramReddy {
+
+	public static void main(String[] args) {
+		
+		
+		String firstName="Bollavaram Sai Haranatha";
+		
+		String lastName="Reddy";
+		System.out.println("Hello "+firstName+" "+lastName);
+	}
+
+}
